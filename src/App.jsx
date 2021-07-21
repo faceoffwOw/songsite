04393822661f21f627ec4a0cec4245c4c83fb299
Ilya <get_right101@mail.ru>
2021-07-21 15:13:39 +0300

    git ignore

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,6 @@ import Gallery from "./templates/Gallery";
 import styled from "styled-components"
 
 
-const AppWrapper = styled.div`
-width: 100%
-min-height: 100vh;
-padding: 20rem;
-background: black;
-`
-
 function App() {
   return (
     <>
