3b7e33976fa62f03b261afd2c9255c502dff2747
Ilya <get_right101@mail.ru>
2021-07-23 14:51:59 +0300

    api

--- src/templates/Galery.jsx
+++ src/templates/Galery.jsx
@@ -2,24 +2,49 @@ import React from "react";
 import "../css/style.css";
 import pic from "../img/s1200.jpg";
 
+const requestUrl = "https://jsonplaceholder.typicode.com/photos";
+
+const arr = [];
+
+fetch(requestUrl)
+    .then(response => response.json())
+    .then(data => {
+      if(data){
+        data.forEach(item => {
+          if(item.albumId === 1){
+            arr.push(item);
+          }
+        });
+      }
+    })
+
+
 const Gallery = () => {
   return (
     <>
       <section>
         <div className="container">
-          <div className="gallery">
+          {arr.map((arr, index) => (
+            <div className="galery">
+            <a href={arr.url}>
+              <img src={arr.url} width="600" height="600"></img>
+            </a>
+            <div className="desc">{arr.title}</div>
+            </div>
+          ))}
+          <div className="galery">
             <a href={pic}>
               <img src={pic} width="600" height="400"></img>
             </a>
             <div className="desc">Add photos</div>
           </div>
-          <div className="gallery">
+          <div className="galery">
             <a href={pic}>
               <img src={pic} width="600" height="400"></img>
             </a>
             <div className="desc">Add photos</div>
           </div>
-          <div className="gallery">
+          <div className="galery">
             <a href={pic}>
               <img src={pic} width="600" height="400"></img>
             </a>
